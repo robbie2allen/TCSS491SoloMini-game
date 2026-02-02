9838a363ecfca1741678fe0b388d1a7e86ed5141
robbie2allen@gmail.com <robbieal@uw.edu>
2026-02-02 11:43:18 -0800

    Added base ECS gameengine
    Added folders, main, index, etc. from team game to build on for my own game.

---
+++ gameengine.js
@@ -0,0 +1,105 @@
+// gameengine.js
+// This game shell was happily modified from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
+
+class GameEngine {
+    constructor(options) {
+        // What you will use to draw
+        // Documentation: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+        this.ctx = null;
+
+        // Everything that will be updated and drawn each frame
+        this.entities = [];
+
+        // ECS Systems
+        this.systems = [];
+
+        // Information on the input
+        this.click = null;
+        this.mouse = null;
+        this.wheel = null;
+        this.keys = {};
+
+        
+
+
+        // Options and the Details
+        this.options = options || {
+            debugging: false,
+        };
+    };
+
+    init(ctx) {
+        this.ctx = ctx;
+        this.startInput();
+        this.timer = new Timer();
+    };
+
+    start() {
+        this.running = true;
+        const gameLoop = () => {
+            this.loop();
+            requestAnimFrame(gameLoop, this.ctx.canvas);
+        };
+        gameLoop();
+    };
+
+    startInput() {
+        const getXandY = e => ({
+            x: e.clientX - this.ctx.canvas.getBoundingClientRect().left,
+            y: e.clientY - this.ctx.canvas.getBoundingClientRect().top
+        });
+        
+        this.ctx.canvas.addEventListener("mousemove", e => {
+            if (this.options.debugging) {
+                console.log("MOUSE_MOVE", getXandY(e));
+            }
+            this.mouse = getXandY(e);
+        });
+
+        this.ctx.canvas.addEventListener("click", e => {
+            if (this.options.debugging) {
+                console.log("CLICK", getXandY(e));
+            }
+            this.click = getXandY(e);
+        });
+
+        this.ctx.canvas.addEventListener("wheel", e => {
+            if (this.options.debugging) {
+                console.log("WHEEL", getXandY(e), e.wheelDelta);
+            }
+            e.preventDefault(); // Prevent Scrolling
+            this.wheel = e;
+        });
+
+        this.ctx.canvas.addEventListener("contextmenu", e => {
+            if (this.options.debugging) {
+                console.log("RIGHT_CLICK", getXandY(e));
+            }
+            e.preventDefault(); // Prevent Context Menu
+            this.rightclick = getXandY(e);
+        });
+
+        this.ctx.canvas.addEventListener("keydown", event => this.keys[event.key] = true);
+        this.ctx.canvas.addEventListener("keyup", event => this.keys[event.key] = false);
+    };
+
+    addEntity(entity) {
+        this.entities.push(entity);
+    };
+
+    addSystem(system) {
+        this.systems.push(system);
+    };
+
+    loop() {
+        this.clockTick = this.timer.tick();
+        
+        // Run all systems
+        for (let system of this.systems) {
+            system.update(this.clockTick, this);
+        }
+        
+        // Remove entities marked for removal
+        this.entities = this.entities.filter(e => !e.removeFromWorld);
+    };
+};
---
+++ gamemanifest.js
@@ -0,0 +1,13 @@
+/**
+ * Game manifest, put asset filepaths here.
+ */
+const GameManifest = {
+  data: [
+    "./assets/sprites/rat.png",
+    "./assets/sprites/spikes.png",
+    "./assets/sprites/prototype_background.png",
+    "./assets/sprites/StatueCatsMainCharacterWReversed.png",
+    "./assets/sprites/StatueCatsTileset.png",
+    "./assets/sprites/platform.png",
+  ],
+};
